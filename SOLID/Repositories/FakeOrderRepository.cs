35adab5972562bb6da82b04cc4296d401a5a59ed
ReajDabi <r.dabi.147242.tc@umindanao.edu.ph>
2026-09-18 18:28:17 +0800

    feat: added a fakeOrderRepo

---
+++ SOLID/Repositories/FakeOrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID.Repositories
+{
+    public class FakeOrderRepository:IOrderRepository
+    {
+        public List<string> SavedEmails { get; } = new List<string>();
+
+        public List<decimal> SavedTotals { get; } = new List<decimal>();
+
+
+
+    public void SaveOrder(string email, decimal total)
+        {
+            SavedEmails.Add(email);
+            SavedTotals.Add(total);
+        }
+
+
+    }
+}
